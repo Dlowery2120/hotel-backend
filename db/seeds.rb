113108c0e6314c071c1e5d40b82c3dbc73302157
Devante Lowery <dlowery2120@gmail.com>
2021-05-18 10:28:36 -0500

    hide API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,30 @@
 User.destroy_all
 
 User.create!(username: "ligmaman", password_digest: "123", first_name: "Devante", last_name: "Lowery")
+User.create!(username: "ligmaman", password_digest: "123", first_name: "Devante", last_name: "Lowery")
+User.create!(username: "ligmaman", password_digest: "123", first_name: "Devante", last_name: "Lowery")
+
+
+require 'byebug'
+require 'rest-client'
+require 'json'
+
+########### PARK SEEDS ##############
+
+# park_response = RestClient.get 'https://developer.nps.gov/api/v1/parks?limit=500&api_key=wYSrUWm0QzfFhZOe2ZhqKexlygpTBfp0N1cJoek5'
+# data = JSON.parse(park_response)
+#     park_data = data['data']
+
+#         park_data.each do |park|
+#             Park.create(
+#                 fullName: park['fullName'] != [] ? park['fullName'] : 'Data Not Available',
+#                 description: park['description'] != [] ? park['description'] : 'Data Not Available',
+#                 latitude: park['latitude'] != [] ? park['latitude'] : 'Data Not Available',
+#                 longitude: park['longitude'] != [] ? park['longitude'] : 'Data Not Available',
+#                 entranceFee: park['entranceFees'] != [] ? park['entranceFees'][0]['cost'] : 'Data Not Available',
+#                 city: park['addresses'] != [] ? park['addresses'][0]['city'] : 'Data Not Available',
+#                 state: park['addresses'] != [] ? park['addresses'][0]['stateCode'] : 'Data Not Available',
+#                 imageUrl: park['images'] != [] ? park['images'][0]['url'] : 'Data Not Available',
+#                 weatherInfo: park['weatherInfo'] != [] ? park['weatherInfo'] : 'Data Not Available'
+#             )
+#         end
