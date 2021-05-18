0198ff1684c858f8f3ea5291979c66838a025882
Devante Lowery <dlowery2120@gmail.com>
2021-05-17 23:09:07 -0500

    edit user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,5 +27,5 @@ class Api::V1::UsersController < ApplicationController
   private 
   
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:username, :password_digest, :password_confirmation, :first_name, :last_name)
   end
