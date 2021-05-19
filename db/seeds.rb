d6628b52110b6620e704e85eaf92d3b742c0e990
Devante Lowery <dlowery2120@gmail.com>
2021-05-19 15:28:28 -0500

    added api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,16 @@
 User.destroy_all
 
-User.create!(username: "ligmaman", password_digest: "123", first_name: "Devante", last_name: "Lowery")
-User.create!(username: "ligmaman", password_digest: "123", first_name: "Devante", last_name: "Lowery")
-User.create!(username: "ligmaman", password_digest: "123", first_name: "Devante", last_name: "Lowery")
 
-
+require 'pry'
+require 'rest-client'
 require 'uri'
 require 'net/http'
 require 'openssl'
 
-url = URI("https://hotels-com-free.p.rapidapi.com/pde/property-details/v1/hotels.com")
+
+User.create!(username: "ligmaman", password_digest: "123", first_name: "Devante", last_name: "Lowery")
+
+url = URI("https://hotels-com-free.p.rapidapi.com/pde/property-details/v1/hotels.com/106346?rooms=1&checkIn=2021-01-27&checkOut=2021-01-28&locale=en_US&currency=USD&include=neighborhood")
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
@@ -20,7 +21,21 @@ request["x-rapidapi-key"] = ENV["KEY"]
 request["x-rapidapi-host"] = 'hotels-com-free.p.rapidapi.com'
 
 response = http.request(request)
-puts response.read_body
+
+array = JSON.parse(response.body)
+
+
+binding.pry
+# GET request with modified headers
+# RestClient.get 'https://hotels-com-free.p.rapidapi.com/pde/property-details/v1/hotels.com', {:Authorization => 'Bearer 0c909ee324msh24ce65d9b57297cp180852jsn5b71f97c1723'}
+
+# POST request with modified headers
+# RestClient.post 'http://example.com/resource', {:foo => 'bar', :baz => 'qux'}, {:Authorization => 'Bearer cT0febFoD5lxAlNAXHo6g'}
+
+# # DELETE request with modified headers
+# RestClient.delete 'http://example.com/resource', {:Authorization => 'Bearer cT0febFoD5lxAlNAXHo6g'}
+
+
 ########### PARK SEEDS ##############
 
 # park_response = RestClient.get 'https://developer.nps.gov/api/v1/parks?limit=500&api_key=wYSrUWm0QzfFhZOe2ZhqKexlygpTBfp0N1cJoek5'
